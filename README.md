e36301108427f094e6d492ebfd599e5edff7b0df
matheusmosca <matheusmoscag@gmail.com>
2020-07-18 14:04:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fly image compressor
+A simple aplication to reduce the size of png and jpg files.
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './base.scss';
+
+function App() {
+  return (
+   <div>Hello world</div>
+  );
+}
+
+export default App;
